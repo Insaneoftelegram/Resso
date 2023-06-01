bd4c3ec8291fcf37cd64a16f6ac205217e05bf6f
Insaneoftelegram <104971406+Insaneoftelegram@users.noreply.github.com>
2023-06-01 21:25:43 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,24 @@
+import re
+
+import sys
+
+import time
+
+from config import (API_ID, API_HASH, BOT_TOKEN)
+
+from pyrogram import Client
+
+StartTime = time.time()
+
+app = Client(
+
+    "Resso",
+
+    API_ID,
+
+    API_HASH,
+
+    bot_token=BOT_TOKEN,
+
+)
+
